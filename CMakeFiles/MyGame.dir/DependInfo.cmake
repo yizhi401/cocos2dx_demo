b59a2034d80c9b9f202f591a32354c6a29b33f29
yizhi401 <yizhi401@126.com>
2014-10-31 16:24:51 +0800

    add new files

---
+++ CMakeFiles/MyGame.dir/DependInfo.cmake
@@ -0,0 +1,55 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/peter/mygit/cocos2dx_demo/Classes/AppDelegate.cpp" "/home/peter/mygit/cocos2dx_demo/CMakeFiles/MyGame.dir/Classes/AppDelegate.cpp.o"
+  "/home/peter/mygit/cocos2dx_demo/Classes/HelloWorldScene.cpp" "/home/peter/mygit/cocos2dx_demo/CMakeFiles/MyGame.dir/Classes/HelloWorldScene.cpp.o"
+  "/home/peter/mygit/cocos2dx_demo/proj.linux/main.cpp" "/home/peter/mygit/cocos2dx_demo/CMakeFiles/MyGame.dir/proj.linux/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "LINUX"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/peter/mygit/cocos2dx_demo/cocos2d/cocos/CMakeFiles/cocos2d.dir/DependInfo.cmake"
+  "/home/peter/mygit/cocos2dx_demo/cocos2d/external/Box2D/CMakeFiles/box2d.dir/DependInfo.cmake"
+  "/home/peter/mygit/cocos2dx_demo/cocos2d/external/tinyxml2/CMakeFiles/tinyxml2.dir/DependInfo.cmake"
+  "/home/peter/mygit/cocos2dx_demo/cocos2d/external/unzip/CMakeFiles/unzip.dir/DependInfo.cmake"
+  "/home/peter/mygit/cocos2dx_demo/cocos2d/external/xxhash/CMakeFiles/xxhash.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include/GLFW"
+  "/usr/include/GLFW"
+  "cocos2d"
+  "cocos2d/cocos"
+  "cocos2d/cocos/audio/include"
+  "cocos2d/cocos/platform"
+  "cocos2d/cocos/platform/desktop"
+  "cocos2d/cocos/platform/linux"
+  "cocos2d/cocos/editor-support"
+  "cocos2d/extensions"
+  "cocos2d/external"
+  "cocos2d/external/edtaa3func"
+  "cocos2d/external/chipmunk/include/chipmunk"
+  "cocos2d/external/jpeg/include/linux"
+  "cocos2d/external/tiff/include/linux"
+  "cocos2d/external/webp/include/linux"
+  "cocos2d/external/tinyxml2"
+  "cocos2d/external/unzip"
+  "cocos2d/external/freetype2/include/linux"
+  "cocos2d/external/websockets/include/linux"
+  "cocos2d/external/spidermonkey/include/linux"
+  "cocos2d/external/linux-specific/fmod/include/64-bit"
+  "cocos2d/external/xxhash"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
