d88535b41d767468471b6d6e4ee90da83e78199d
yizhi401 <yizhi401@126.com>
2014-10-31 17:43:57 +0800

    chenhao

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -1,6 +1,8 @@
 #include "HelloWorldScene.h"
+#include <iostream>
 
 USING_NS_CC;
+using namespace std;
 
 Scene* HelloWorld::createScene()
 {
@@ -54,14 +56,20 @@ bool HelloWorld::init()
     // add a label shows "Hello World"
     // create and initialize a label
     
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-    
+    auto label = Label::createWithTTF("Hello Guys", "fonts/Marker Felt.ttf", 48);
+    auto subTitle = Label::createWithTTF("This is a picture that I want you show you, enjoy!","fonts/Marker Felt.ttf",24);
+    cout << "坐标值y  ="<<origin.y<<"坐标值x=" << origin.x;
+    auto titlePosition =Vec2(origin.x + visibleSize.width/2,origin.y + visibleSize.height - label->getContentSize().height) ;
     // position the label on the center of the screen
-    label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                            origin.y + visibleSize.height - label->getContentSize().height));
+    label->setPosition(titlePosition);
+    
+    auto subTitlePosition = Vec2(200,100);
+    
+    subTitle->setPosition(subTitlePosition);
 
     // add the label as a child to this layer
     this->addChild(label, 1);
+    this->addChild(subTitle,0);
 
     // add "HelloWorld" splash screen"
     auto sprite = Sprite::create("HelloWorld.png");
@@ -69,9 +77,13 @@ bool HelloWorld::init()
     // position the sprite on the center of the screen
     sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
 
+    auto backgroundSprite = Sprite::create("background.jpg");
     // add the sprite as a child to this layer
-    this->addChild(sprite, 0);
-    
+//    this->addChild(sprite, 0);
+//    backgroundSprite->setPosition(Vec2(visibleSize.width/2 + origin.x,visibleSize.height/2+origin.y));
+//    backgroundSprite->setContentSize(Size(visibleSize.width,visibleSize.height));
+    backgroundSprite->setPosition(Vec2(visibleSize/2) + origin);
+    this->addChild(backgroundSprite);
     return true;
 }
 
